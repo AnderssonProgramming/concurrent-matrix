18dd5da221ea75fda9b642cfe7f00da7da4df39a
AnderProgramming <158221956+AnderssonProgramming@users.noreply.github.com>
2025-06-26 18:54:15 -0500

    feat: add concurrent-matrix project scaffolding

---
+++ src/main/java/edu/eci/arsw/concurrent_matrix/ConcurrentMatrixApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.concurrent_matrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrentMatrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrentMatrixApplication.class, args);
+	}
+
+}
